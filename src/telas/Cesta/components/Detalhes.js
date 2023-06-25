a77620ea6cc10fc8839a521eda46a16d91159117
Mateus Palmeira <jmateusps16@gmail.com>
2023-06-25 15:01:56 -0300

    componente botao estilizado

--- src/telas/Cesta/components/Detalhes.js
+++ src/telas/Cesta/components/Detalhes.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Image, StyleSheet, View } from "react-native";
 
 import Texto from "../../../components/Texto";
+import Botao from "../../../components/Botao";
 
-export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco }) {
+export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco, botao }) {
     return <>
         <Texto style={estilos.nome}> { nome } </Texto>
         <View style={estilos.fazenda}>
@@ -12,6 +13,8 @@ export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, pr
         </View>
         <Texto style={estilos.descricao}>{ descricao }</Texto>
         <Texto style={estilos.preco}>{ preco }</Texto>
+
+        <Botao texto={botao} style={estilos.botao} onPress={() => {console.log("Clicou em comprar")}} />
     </>
 }
 
@@ -46,5 +49,8 @@ const estilos = StyleSheet.create({
         fontSize: 26,
         lineHeight: 42,
         marginTop: 8,
-    }
+    },
+    botao: {
+        marginTop: 16,
+    },
 });
