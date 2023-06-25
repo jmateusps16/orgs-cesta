671680b69dcdd41ea71f5c63615967534c8cb1f5
Mateus Palmeira <jmateusps16@gmail.com>
2023-06-25 10:44:47 -0300

    adicionando fonts

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import { 
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold
+} from '@expo-google-fonts/montserrat';
 
 import Cesta from './src/telas/Cesta';
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if (!fonteCarregada) {
+    return <View />
+  }
+
   return (
-    <View>
+    <SafeAreaView>
       <StatusBar />
       <Cesta />
-    </View>
+    </SafeAreaView>
   );
 }
