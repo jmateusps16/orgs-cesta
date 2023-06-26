cdabe9d2942516f1c0e2ba0014ad32221357e43c
Mateus Palmeira <jmateusps16@gmail.com>
2023-06-26 08:16:18 -0300

    refatorando com o uso de FlatList

--- src/telas/Cesta/components/Item.js
+++ src/telas/Cesta/components/Item.js
@@ -3,32 +3,21 @@ import React from "react";
 import Texto from "../../../components/Texto";
 import { Image, StyleSheet, View } from "react-native";
 
-export default function Itens( {titulo, lista }) {
-    return <>
-    <Texto style={estilos.titulo}>{titulo}</Texto>
-    {lista.map(({nome, imagem}) => {
-        return <View key={nome} style={estilos.item}>
-            <Image source={imagem} style={estilos.imagem}/>
-            <Texto style={estilos.nome}>{nome}</Texto>
-        </View>
-    })}
-    </>
+export default function Item({ item: {nome, imagem}}) {
+
+    return <View key={nome} style={estilos.item}>
+        <Image source={imagem} style={estilos.imagem} />
+        <Texto style={estilos.nome}>{nome}</Texto>
+    </View>
 }
 
 const estilos = StyleSheet.create({
-    titulo: {
-        color: "#464646",
-        fontWeight: "bold",
-        marginTop: 32,
-        marginBottom: 8,
-        fontSize: 20,
-        lineHeight: 32,
-    },
     item: {
         flexDirection: "row",
         borderBottomWidth: 1,
         borderBottomColor: "#ECECEC",
         paddingVertical: 16,
+        marginHorizontal: 16,
         alignItems: "center",
     },
     imagem: {
--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,22 +1,42 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
+
 import Topo from './components/Topo';
 import Detalhes from './components/Detalhes';
-import Itens from './components/Itens';
+import Item from './components/Item';
+import Texto from '../../components/Texto';
 
 export default function Cesta({ topo, detalhes, itens }) {
-    return <ScrollView>
-        <Topo {...topo} />
-        <View style={estilos.cesta}>
-            <Detalhes {...detalhes} />
-            <Itens {...itens}/>
-        </View>
-    </ScrollView>
+    return <>
+        <FlatList
+            data={itens.lista}
+            renderItem={Item}
+            keyExtractor={({ nome }) => nome}
+            ListHeaderComponent={() => {
+                return <>
+                    <Topo {...topo} />
+                    <View style={estilos.cesta}>
+                        <Detalhes {...detalhes} />
+                    </View>
+                    <Texto style={estilos.titulo}>{itens.titulo}</Texto>
+                </>
+            }}
+        />
+    </>
 }
 
 const estilos = StyleSheet.create({
     cesta: {
         paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    titulo: {
+        color: "#464646",
+        fontWeight: "bold",
+        marginTop: 32,
+        marginBottom: 8,
+        fontSize: 20,
+        lineHeight: 32,
         paddingHorizontal: 16,
     },
 });
